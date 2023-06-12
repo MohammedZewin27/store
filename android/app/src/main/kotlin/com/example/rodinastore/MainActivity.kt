35607d01f422c122cae3070bce26145f0d8bee14
Amar <mohamed.zewin27@gmail.com>
2023-06-12 10:15:50 +0300

    .

---
+++ android/app/src/main/kotlin/com/example/rodinastore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rodinastore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/layout/homeLayout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeLayoutScreen extends StatelessWidget {
+  const HomeLayoutScreen({super.key});
+static const String routeName='HomeLayoutScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
